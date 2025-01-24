edd30d63110e8169bfa2afd382814ab16cd97bce
Danie1Arias <daniel.arias@estudiants.urv.cat>
2025-01-24 16:11:05 +0100

    Delete order crossover function

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -17,20 +17,6 @@ class GeneticAlgorithm:
         self.mutation_scheme = mutation_scheme
 
     # Crossover functions
-    def order_crossover(self, parent1, parent2):
-        size = len(parent1)
-        start, end = sorted(random.sample(range(size), 2))
-        child = [None] * size
-        child[start:end] = parent1[start:end]
-        pointer = end
-        for gene in parent2:
-            if gene not in child:
-                if pointer >= size:
-                    pointer = 0
-                child[pointer] = gene
-                pointer += 1
-        return child
-    
     def one_point_crossover(self, parent1, parent2):
         size = len(parent1)
         point = random.randint(1, size - 1)
